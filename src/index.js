81b2f7e5b428498357923e970ab64ba0f2425830
Nazar Leshchyshyn <lesh1.nazar@gmail.com>
2020-10-26 15:27:52 +0200

    c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './styles.css';
+
+
+function sortByLastName(books) {
+    return books.sort((a, b) => {
+        console.log(a.author.split(' ')[1],b.author.split(' ')[1]);
+     return a.author.split(' ')[1] - b.author.split(' ')[1]
+  })
+}
+
+
+console.log(sortByLastName([
+  { name: "Harry Potter", rating: "8+", author: "Joanne Rowling" },
+  { name: "Warcross", rating: "13+", author: "Marie Lu" },
+  { name: "The Hunger Games", rating: "12+", author: "Suzanne Collins" },
+]));
